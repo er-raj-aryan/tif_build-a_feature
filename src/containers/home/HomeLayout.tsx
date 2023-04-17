fe9b4d128317cc08bc5ac1f885a922ff1793e4bf
Raj Aryan <raj@dailycutting.com>
2023-04-17 14:19:28 +0530

    because:assigment completed

---
+++ src/containers/home/HomeLayout.tsx
@@ -0,0 +1,88 @@
+import {
+  Container,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Heading,
+  TabProps,
+  Box,
+  Grid,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import InterviewSettingsForm from "./InterviewSettingsForm";
+import JobDetailsForm from "./JobDetailsForm";
+import RequisitionForm from "./RequisitionDetailsForm";
+import DisplayCard from "./PreviewCard";
+import { PageNumbers } from "../../interface/home";
+import { IInterViewSettings, IJobDetails, IRequisitionDetails } from "@src/interface/forms";
+import { Console } from "console";
+
+const CustomTab: React.FC<TabProps> = ({ children, ...props }) => {
+  return (
+    <Tab p="1rem" fontFamily="Poppins" {...props}>
+      {children}
+    </Tab>
+  );
+};
+
+const HomeLayout = () => {
+  const [page, setPage] = useState<PageNumbers>(0);
+  const [RequisitionDetailsFormData, setRequisitionDetailsFormData] = useState<IRequisitionDetails>();
+  const [JobDetailsFormData, setJobDetailsFormData] = useState<IJobDetails>();
+  const [InterviewSettingsFormData, setInterviewSettingsFormData] = useState<IInterViewSettings>();
+
+  const handlePage = (pageNumber: PageNumbers) => {
+    setPage(pageNumber);
+  };
+
+  const handleRequisitionFormData = (pData:IRequisitionDetails) => {
+    setRequisitionDetailsFormData(pData);
+  }
+  const handleJobDetailsFormData = (pData:IJobDetails) => {
+    setJobDetailsFormData(pData);
+  }
+  const handleInterviewSettingsFormData = (pData:IInterViewSettings) => {
+    setInterviewSettingsFormData(pData);
+  }
+
+
+
+  return (
+    <Box w="100%">
+      <Container maxW="1200px">
+        <Heading fontFamily="Poppins" fontSize="1.5rem" my="2rem">
+          Create Candidate Requisition
+        </Heading>
+        <Tabs index={page} isLazy lazyBehavior="keepMounted">
+          <TabList>
+            <CustomTab>Requistion Details</CustomTab>
+            <CustomTab>Job Details</CustomTab>
+            <CustomTab>Interview Settings</CustomTab>
+          </TabList>
+          <Grid display="grid" gridTemplateColumns="3fr 2fr" gap="24px">
+            <TabPanels>
+              <TabPanel>
+                <RequisitionForm handleTab={handlePage} handleRequisitionFormData={handleRequisitionFormData} />
+              </TabPanel>
+              <TabPanel>
+                <JobDetailsForm handleTab={handlePage} handleJobDetailsFormData={handleJobDetailsFormData} />
+              </TabPanel>
+              <TabPanel>
+                <InterviewSettingsForm handleTab={handlePage} handleInterviewSettingsFormData={handleInterviewSettingsFormData} />
+              </TabPanel>
+            </TabPanels>
+            <DisplayCard 
+            requisitionDetails={RequisitionDetailsFormData} 
+            jobDetails={JobDetailsFormData}
+            interviewSettings={InterviewSettingsFormData}
+            />
+          </Grid>
+        </Tabs>
+      </Container>
+    </Box>
+  );
+};
+
+export default HomeLayout;
